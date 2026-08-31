c534e6289a97d41a10d74a5d102aa0688064c742
therivernix <therivernix@tommygeusens.com>
2026-08-31 20:38:02 +0200

    Adding homebrew

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,6 +34,23 @@ rm -rf /usr/share/gnome-shell/extensions/window-list@gnome-shell-extensions.gcam
 # Compile GSettings schemas for GNOME extensions
 find /usr/share/gnome-shell/extensions -type d -name schemas \
     -exec glib-compile-schemas {} \;
+    
+    
+# Install homebrew
+# Copy Homebrew files from the brew image
+# And enable
+COPY --from=ghcr.io/ublue-os/brew:latest /system_files /
+RUN --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /usr/bin/systemctl preset brew-setup.service && \
+    /usr/bin/systemctl preset brew-update.timer && \
+    /usr/bin/systemctl preset brew-upgrade.timer
+    
+    
+# source https://github.com/ublue-os/brew/pkgs/container/brew
+
+
 
 #### Example for enabling a System Unit File
 
